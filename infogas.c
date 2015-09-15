c6481323f3fabe28ee8d5d0390a68197b52bf685
Antonio Francelino <antoniofrancelinoneto@hotmail.com>
2015-09-15 19:11:09 -0300

    inicio do programa

--- infogas.c
+++ infogas.c
@@ -2,8 +2,14 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+
 int main()
 {
+    printf("programa referencial para abastecimento:");
+    printf("\nObs: o tempo do programa deve entrar em forma de\n HH:MM no formato de 24h");
+    char escolha,sair,timein[6];
+    float litros;
+    float cosumo;
     float km,hr,t,lf,lp,ci,cf,sd,pd,vf,vp;
 return 0;
 }
