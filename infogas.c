14686f9ef56fe2c4337964373bd5cfee1eb4184f
Silas Wesley Maia Marinho <silas.wesley@hotmail.com>
2015-09-15 19:35:17 -0300

    Chars adicionados

--- infogas.c
+++ infogas.c
@@ -7,10 +7,12 @@ int main()
 {
     printf("programa referencial para abastecimento:");
     printf("\nObs: o tempo do programa deve entrar em forma de\n HH:MM no formato de 24h");
-    char escolha,sair,timein[6];
+    char escolha,sair,timein [];
     float litros;
     float cosumo;
     float km,hr,t,lf,lp,ci,cf,sd,pd,vf,vp;
+    float velocidade;
+    char tempo [];
 return 0;
 }
 
