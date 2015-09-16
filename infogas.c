63baa01f2afcce4ff4bbfdfed34a7e182e2bc8a6
Thiago Queiroz <thiago_queiroz94@outlook.com.br>
2015-09-16 15:11:27 -0300

    incluão de alguns printfs e scanfs

--- infogas.c
+++ infogas.c
@@ -7,5 +7,8 @@ int main(void)
     float n,hi,h,hn,mini,min,minn,p,gi,g,vp,vmf,gc,odo,odoi,i,km;
     printf("\n\nINTRUCOES:\n");
     printf("\n1-A hora de ver se coloca n:n(EX:10:30).\n");
-
+    printf("2-Odometro deve esta em km.\n");
+    printf("3-O volume do tanque deve estar em L\n");
+    printf("Digite o numero de paradas que você fara no caminho:\n");
+    scanf("%f",&n);
 }
