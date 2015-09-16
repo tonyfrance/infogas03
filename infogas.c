a662b49cb60287de4a3a5518a37f60970f8ce50d
Wirlany Karla Nascimento De Araujo Veloso <wirlanyaraujo@gmail.com>
2015-09-16 14:56:49 -0300

    alteração de printf

--- infogas.c
+++ infogas.c
@@ -3,7 +3,9 @@
 
 int main(void)
 {
+    
     float n,hi,h,hn,mini,min,minn,p,gi,g,vp,vmf,gc,odo,odoi,i,km;
     printf("\n\nINTRUCOES:\n");
+    printf("\n1-A hora de ver se coloca n:n(EX:10:30).\n");
 
 }
