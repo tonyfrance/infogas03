e48c1e6447183d4da72b3c634df00c5b63228b08
Joao Gabriel Gouveia De Souza Brito <gabriel.gouveia@live.com>
2015-09-16 01:08:49 -0300

    Ajuste no programa, realocacao e uso de variaveis

--- infogas.c
+++ infogas.c
@@ -1,29 +1,39 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include <unistd.h>
+
 
 int main()
 {
     printf("programa referencial para abastecimento:");
     printf("\nObs: o tempo do programa deve entrar em forma de\n HH:MM no formato de 24h");
-    char escolha,sair,timein [];
+    char escolha,sair,timeini[6];
+    struct lkt{
     float litros;
-    float cosumo;
+    float consumo;
+    float velocidade;
+    char tempo[6];
+    };
+    struct lkt viagem;
     float km,hr,t,lf,lp,ci,cf,sd,pd,vf,vp;
     float velocidade;
-    char tempo [];
-return 0;
-    };
-    struct lkt control[];
+    
+
+    
     printf("\nDigite o valor inicial do Odometro: ");
     float kmi,disttotal; //quilometro inicial
     scanf("%f",&kmi);
     
     printf("\nDigite o instante de tempo inicial em HH:MM : ");
-    scanf("%s", &timeni);
-    char horasref[], minutosref[];//variaveis intermediarias,conversão str to int
+    scanf("%s", &timeini);
+    char horasref[3], minutosref[3];//variaveis intermediarias,conversão str to int
     int i=0;
-    for(i=0;i<2;i++);
     char horasref[3], minutosref[3];
-    int i=0;
+    for(i=0;i<2;i++)
+    {
+        horasref[i]=timeini[i];
+        minutosref[i]=timerini[(3+i)];
+    }
+    horasref[2]='\0';
+    minutosref[2]='\0';
+}
