e2034d77c0be5fa15c2684b8df6fd4ab6b560d58
Silas Wesley Maia Marinho <silas.wesley@hotmail.com>
2015-09-16 11:10:24 -0300

    Inclusão de Bibliotecas e mais linhas

--- infogas.c
+++ infogas.c
@@ -1,22 +1,29 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-
+#include <unistd.h>
 
 int main()
 {
     printf("programa referencial para abastecimento:");
     printf("\nObs: o tempo do programa deve entrar em forma de\n HH:MM no formato de 24h");
     char escolha,sair,timeini[6];
-    struct lkt;
-    float litros;
-    float consumo;
-    float velocidade;
-    char tempo[];
-    struct lkt viagem;
-    float km,hr,t,lf,lp,ci,cf,sd,pd,vf,vp;
-    float velocidade;
-    
+    struct lkt{  //registro para poder guardar todos os valores de parciais.
+        float litros;
+        float consumo;
+        float velocidade;
+        char tempo[];
+  };
+    struct lkt control[];
+    printf("\nDigite o valor inicial do Odometro:  ");
+    float kmi, disttotal; //quilometro inicial
+    scanf("%f", &kmi);
+
+    printf("\nDigite o instante de tempo inicial em HH:MM : ");
+    scanf("%s", &timeini);
+    char horasref[], minutosref[]; //variaveis intermediarias, conversao str to int
+    int i=0;
+    for(i=0;i<2;i++) 
 
     
     printf("\nDigite o valor inicial do Odometro: ");
